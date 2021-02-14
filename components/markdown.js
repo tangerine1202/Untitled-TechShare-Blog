be8bc1298007507f004bb98a415f7d290c666ea2
Alan Huang <alan0988469026@gmail.com>
2021-02-14 19:10:52 +0800

    fix: back to home btn

--- components/markdown.js
+++ components/markdown.js
@@ -2,7 +2,7 @@ import ReactMarkdown from 'react-markdown';
 
 export default function Markdown({ content }) {
   return (
-    // eslint-disable-next-line react/no-children-prop
-    <ReactMarkdown children={content} className="prose p-2 sm:prose-sm xl:prose-lg mx-auto" />
+    /* eslint-disable-next-line react/no-children-prop */
+    <ReactMarkdown children={content} className="prose sm:prose-sm xl:prose-lg mx-auto" />
   );
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -25,9 +25,12 @@ export default function Post({ data }) {
     <Layout>
       <Markdown content={data} />
       <Link href="/">
-        <a>
-          <p>Back to Home page</p>
-        </a>
+        <button
+          type="submit"
+          className="flex justify-center mx-auto mt-6 mb-4 py-2 px-4 border-2 border-yellow-500 rounded-lg font-medium text-yellow-500 bg-transparent hover:bg-yellow-300 hover:bg-opacity-20 hover"
+        >
+          Back to Home page
+        </button>
       </Link>
     </Layout>
   );
