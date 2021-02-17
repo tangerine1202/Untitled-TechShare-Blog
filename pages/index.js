fba88a94efa661cd6e5bd379cec8f198dc50d2de
Ice1187 <Icing1187@gmail.com>
2021-02-18 00:52:20 +0800

    feat: add dark mode to index.js

--- pages/index.js
+++ pages/index.js
@@ -27,23 +27,27 @@ class Home extends React.Component {
     const { total, data } = postList;
 
     return (
-      <div className={isDarkMode ? 'dark' : 'light'}>
+      <div className={`transform duration-300 ${isDarkMode && 'dark bg-gray-800'}`}>
         <Head>
           <title>Untitled TechShare Blog</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <Layout>
+          <div className="flex justify-end items-center p-8">
+            <label htmlFor="darkModeToggle">
+              <div className="flex items-center bg-gray-300 rounded-full w-12 h-6 p-1">
+                <div className={`bg-white rounded-full shadow-md w-5 h-5 transform duration-300 ease-in-out ${isDarkMode && 'translate-x-5'}`} />
+              </div>
+              <input id="darkModeToggle" type="checkbox" className="hidden" onClick={() => this.setState({ isDarkMode: !isDarkMode })} />
+            </label>
+          </div>
+
           <div className="font-huninn px-4 h-screen flex flex-col justify-center select-none">
 
-            <div>
-              <span className="text-yellow-500 dark:text-purple-500">Dark Mode</span>
-              <input type="checkbox" onClick={() => this.setState({ isDarkMode: !isDarkMode })} />
-            </div>
-
             <h1 className="block md:block text-4xl md:text-6xl text-left md:text-center font-semibold">
-              <span className="block md:inline">Welcome to </span>
-              <span className="text-yellow-500">Untitled TechShare</span>
+              <span className="block md:inline dark:text-white transition-colors duration-300">Welcome to </span>
+              <span className="text-yellow-500 dark:text-purple-500 transition-colors duration-300">Untitled TechShare</span>
             </h1>
 
             <p className="mt-8 text-left md:text-center text-lg md:text-2xl text-gray-500 font-normal">
